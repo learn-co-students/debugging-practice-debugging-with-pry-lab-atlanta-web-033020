f40fb3fa0ed486b25843971fe0af13f42549c73f
Geoffrey Harrison <harrisongeo87@gmail.com>
2020-03-29 19:15:18 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,7 +1,13 @@
+require "pry"
+
 def snake_it_up(string)
   if string[0] == "s"
-  10 * "s" + string
+    10.times do
+      string = "s" + string
+    end
+  #10 * "s" + string
   else
   string
   end
+  string
 end
